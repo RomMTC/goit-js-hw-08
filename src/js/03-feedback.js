2aad6408a946d3ef17025c8474cc7cb42ed3c3cc
Roman Lizunov <rlizunoff@gmail.com>
2022-07-31 14:39:46 +0300

    update 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+// Выполняй это задание в файлах 03-feedback.html и 03-feedback.js. Разбей его на несколько подзадач:
 
+// Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message,
+//     в которых сохраняй текущие значения полей формы.Пусть ключом для хранилища будет строка "feedback-form-state".
+// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. 
+// В противном случае поля должны быть пустыми.
+// При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email,
+//     message и текущими их значениями в консоль.
+// Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. 
+// Для этого добавь в проект и используй библиотеку lodash.throttle.
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form');
+const formData = {};
+formRef.addEventListener('input', throttle(onInputChange, 500));
+formRef.addEventListener('submit', onSubmitForm);
+
+dataFromLocalStorage();
+
+function onInputChange(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    // console.log(e.target.value);
+};
+
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+};
