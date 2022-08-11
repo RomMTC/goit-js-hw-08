895baba0b0e6e2786f333c4ae241da79c4ac772b
Roman Lizunov <rlizunoff@gmail.com>
2022-08-11 17:21:39 +0300

    uPDATE 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,31 +5,12 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
+if(localStorage.getItem('videoplayer-current-time')) {
+        player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
 
-// player.getCurrentTime().then(function(seconds) {
-//     // seconds = the current playback position
-// }).catch(function(error) {
-//     // an error occurred
-// });
-
-// const onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
-
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('timeupdate', throttle(onPlay, 1000));
